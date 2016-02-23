bd352ea046285c1060a34ca1956564b8ea48a4b5
Michael S. Huang <mhuang@marinsoftware.com>
2016-02-23 19:26:52 +0800

    add java-pi-kamon project

---
+++ akka-tutorials/java-pi-kamon/src/main/java/akka/tutorial/pi/Pi.java
@@ -0,0 +1,65 @@
+package v.a.akka.tutorial.pi;
+
+import v.a.akka.tutorial.pi.actors.Listener;
+import v.a.akka.tutorial.pi.actors.Master;
+import v.a.akka.tutorial.pi.messages.Calculate;
+import akka.actor.ActorRef;
+import akka.actor.ActorSystem;
+import akka.actor.Props;
+import akka.japi.Creator;
+import kamon.Kamon;
+
+public class Pi {
+    public static void main(String[] args) {
+        Kamon.start();
+
+        try {
+            Thread.sleep(3000);
+        } catch(Exception e) { }
+
+        Pi pi = new Pi();
+        pi.calculate(10, 1000000, 1000000);
+
+        // This application wont terminate unless you shutdown Kamon.
+        Kamon.shutdown();
+    }
+
+    // actors and messages ...
+
+    public void calculate(final int nrOfWorkers, final int nrOfMessages, final int nrOfElements) {
+        // Create an Akka system
+        ActorSystem system = ActorSystem.create("PiSystem");
+
+        // create the result listener, which will print the result and shutdown the system
+        final ActorRef listener = system.actorOf(Props.create(Listener.class), "listener");
+
+        // create the master
+        ActorRef master = system.actorOf(Props.create(new MasterActorCreator(nrOfWorkers, nrOfMessages, nrOfElements, listener)), "master");
+
+        // start the calculation
+        master.tell(new Calculate(), null);
+
+    }
+    
+    
+    static class MasterActorCreator implements Creator<Master> {
+        private final int nrOfWorkers;
+        private final int nrOfMessages;
+        private final int nrOfElements;
+
+        private final ActorRef listener;
+        public MasterActorCreator(final int nrOfWorkers, int nrOfMessages, int nrOfElements, ActorRef listener) {
+            this.nrOfWorkers = nrOfWorkers;
+            this.nrOfMessages = nrOfMessages;
+            this.nrOfElements = nrOfElements;
+            this.listener = listener;
+        }
+        
+        
+        @Override public Master create() {
+          return new Master(nrOfWorkers, nrOfMessages, nrOfElements, listener);
+        }
+      }
+}
+    
+   
---
+++ akka-tutorials/java-pi-kamon/src/main/java/akka/tutorial/pi/actors/Worker.java
@@ -0,0 +1,33 @@
+package v.a.akka.tutorial.pi.actors;
+
+import v.a.akka.tutorial.pi.messages.Result;
+import v.a.akka.tutorial.pi.messages.Work;
+import akka.actor.UntypedActor;
+
+import java.util.Random;
+
+public class Worker extends UntypedActor {
+    
+    private double calculatePiFor(int start, int nrOfElements) {
+        double acc = 0.0;
+        for (int i = start * nrOfElements; i <= ((start + 1) * nrOfElements - 1); i++) {
+          acc += 4.0 * (1 - (i % 2) * 2) / (2 * i + 1);
+        }
+
+        try {
+            Thread.sleep(new Random().nextInt(100));
+        } catch(Exception e) { }
+
+        return acc;
+      }
+   
+    public void onReceive(Object message) {
+      if (message instanceof Work) {
+        Work work = (Work) message;
+        double result = calculatePiFor(work.getStart(), work.getNrOfElements());
+        getSender().tell(new Result(result), getSelf());
+      } else {
+        unhandled(message);
+      }
+    }
+  }
