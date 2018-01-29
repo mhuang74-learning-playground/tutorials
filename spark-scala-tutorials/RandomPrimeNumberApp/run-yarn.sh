9d4abd08ee4ec74f639b610be2afc1853134a5c8
Michael S. Huang <mhuang@marinsoftware.com>
2018-01-29 23:40:32 +0800

    set runtime spark config

--- spark-scala-tutorials/RandomPrimeNumberApp/run-yarn.sh
+++ spark-scala-tutorials/RandomPrimeNumberApp/run-yarn.sh
@@ -3,11 +3,12 @@ sbt assembly
 spark-submit --class SimpleRandomPrimeNumberApp \
     --master yarn \
     --deploy-mode cluster \
-    --driver-memory 4g \
-    --executor-memory 2g \
-    --executor-cores 1 \
-    --queue root.prioritySpark \
+    --driver-memory 2g \
+    --executor-memory 8g \
+    --executor-cores 4 \
+    --num-executors 2 \
+    --queue root.users.mhuang \
     --conf spark.yarn.archive=hdfs:///user/mhuang/lib/spark-libs-2.2.0.jar \
     target/scala-2.11/RandomPrimeNumberApp-assembly-1.0.jar \
-    100000 \
+    1000000 \
     nodebug
