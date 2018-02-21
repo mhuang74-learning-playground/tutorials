8279378d1cea035af4e78c292ac3b0f1b15209bd
Michael S. Huang <mhuang@marinsoftware.com>
2018-02-22 01:22:43 +0800

    enable log4j logging on yarn; add debug traces to dump info about partititions

--- spark-scala-tutorials/RandomPrimeNumberApp/run-local.sh
+++ spark-scala-tutorials/RandomPrimeNumberApp/run-local.sh
@@ -1,3 +1,18 @@
 #!/bin/bash
 sbt clean assembly
-spark-submit --master=local[4] --class SimpleRandomPrimeNumberApp target/scala-2.11/RandomPrimeNumberApp-assembly-1.0.jar 100000 nodebug
+spark-submit --verbose --class SimpleRandomPrimeNumberApp \
+    --master=local[8] \
+    --deploy-mode client \
+    --driver-memory 2g \
+    --executor-memory 2g \
+    --executor-cores 3 \
+    --num-executors 5 \
+    --driver-class-path target/scala-2.11/RandomPrimeNumberApp-assembly-1.0.jar \
+    --driver-java-options "-Dlog4j.configuration=log4j.properties" \
+    --conf "spark.executor.extraJavaOptions=-Dlog4j.configuration=log4j.properties" \
+    --conf "spark.serializer=org.apache.spark.serializer.KryoSerializer" \
+    --conf "spark.io.compression.codec=snappy" \
+    target/scala-2.11/RandomPrimeNumberApp-assembly-1.0.jar \
+    100000 \
+    8 \
+    debug
