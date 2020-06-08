632e94895802496e81c03d1ecdd4007d44ce727a
Michael S. Huang <mhuang74@gmail.com>
2020-06-08 16:25:33 +0800

    refactor LEDC setup outside of app_main()

--- esp-tutorial/simple_led_pwm/main/simple_led_pwd_main.c
+++ esp-tutorial/simple_led_pwm/main/simple_led_pwd_main.c
@@ -43,55 +43,63 @@ void init(void)
 
 }
 
-void app_main(void)
-{
-    int ch;
+/*
+    * Prepare and set configuration of timers
+    * that will be used by LED Controller
+    */
+ledc_timer_config_t ledc_timer = {
+    .duty_resolution = LEDC_TIMER_8_BIT, // resolution of PWM duty
+    .freq_hz = 4000,                      // frequency of PWM signal
+    .speed_mode = LEDC_HS_MODE,           // timer mode
+    .timer_num = LEDC_HS_TIMER,            // timer index
+    .clk_cfg = LEDC_AUTO_CLK,              // Auto select the source clock
+};
 
-    init();
 
-    /*
-     * Prepare and set configuration of timers
-     * that will be used by LED Controller
-     */
-    ledc_timer_config_t ledc_timer = {
-        .duty_resolution = LEDC_TIMER_8_BIT, // resolution of PWM duty
-        .freq_hz = 4000,                      // frequency of PWM signal
-        .speed_mode = LEDC_HS_MODE,           // timer mode
-        .timer_num = LEDC_HS_TIMER,            // timer index
-        .clk_cfg = LEDC_AUTO_CLK,              // Auto select the source clock
-    };
+/*
+    * Prepare individual configuration
+    * for each channel of LED Controller
+    * by selecting:
+    * - controller's channel number
+    * - output duty cycle, set initially to 0
+    * - GPIO number where LED is connected to
+    * - speed mode, either high or low
+    * - timer servicing selected channel
+    *   Note: if different channels use one timer,
+    *         then frequency and bit_num of these channels
+    *         will be the same
+    */
+ledc_channel_config_t ledc_channel[LEDC_TEST_CH_NUM] = {
+    {
+        .channel    = LEDC_HS_CH0_CHANNEL,
+        .duty       = 0,
+        .gpio_num   = LEDC_HS_CH0_GPIO,
+        .speed_mode = LEDC_HS_MODE,
+        .hpoint     = 0,
+        .timer_sel  = LEDC_HS_TIMER
+    },
+};
+
+int ch;
+
+
+void setup(void) {
     // Set configuration of timer0 for high speed channels
     ledc_timer_config(&ledc_timer);
 
-    
-    /*
-     * Prepare individual configuration
-     * for each channel of LED Controller
-     * by selecting:
-     * - controller's channel number
-     * - output duty cycle, set initially to 0
-     * - GPIO number where LED is connected to
-     * - speed mode, either high or low
-     * - timer servicing selected channel
-     *   Note: if different channels use one timer,
-     *         then frequency and bit_num of these channels
-     *         will be the same
-     */
-    ledc_channel_config_t ledc_channel[LEDC_TEST_CH_NUM] = {
-        {
-            .channel    = LEDC_HS_CH0_CHANNEL,
-            .duty       = 0,
-            .gpio_num   = LEDC_HS_CH0_GPIO,
-            .speed_mode = LEDC_HS_MODE,
-            .hpoint     = 0,
-            .timer_sel  = LEDC_HS_TIMER
-        },
-    };
 
     // Set LED Controller with previously prepared configuration
     for (ch = 0; ch < LEDC_TEST_CH_NUM; ch++) {
         ledc_channel_config(&ledc_channel[ch]);
     }
+}
+
+void app_main(void)
+{
+
+    init();
+
+    setup();
 
 
     int duty = 0;
