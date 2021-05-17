c20fa5c5b56f5956b6f155276d016e6c47b35bb6
Michael S. Huang <mhuang74@gmail.com>
2021-05-17 15:24:28 +0800

    unbounded multi-thread impl

--- rust-tutorials/simple_webserver/src/main.rs
+++ rust-tutorials/simple_webserver/src/main.rs
@@ -15,7 +15,9 @@ fn main() {
 	for stream in listener.incoming() {
 		let stream = stream.unwrap();
 
-		handle_connection(stream);
+		thread::spawn(|| {
+			handle_connection(stream);
+		});
 	}
 
     fn handle_connection(mut stream: TcpStream) {
@@ -24,19 +26,21 @@ fn main() {
 
     	stream.read(&mut buffer).unwrap();
 
-	    println!("Request: {}", String::from_utf8_lossy(&buffer[..]));
+	    println!("Thread: {:?} - Request: {}", thread::current().id(), String::from_utf8_lossy(&buffer[..]));
 
     	let get = b"GET / HTTP/1.1\r\n";
     	let sleep = b"GET /sleep HTTP/1.1\r\n";
 
-    	let (status_line, filename) = if buffer.starts_with(get) {
-    		("HTTP/1.1 200 OK", "hello.html")
-    	} else if buffer.starts_with(sleep) {
-    		thread::sleep(Duration::from_secs(5));
-    		("HTTP/1.1 200 OK", "hello.html")
-    	} else {
-    		("HTTP/1.1 404 NOT FOUND", "404.html")
-    	};
+    	let (status_line, filename) = 
+	    	if buffer.starts_with(get) {
+	    		("HTTP/1.1 200 OK", "hello.html")
+	    	} else if buffer.starts_with(sleep) {
+	    		thread::sleep(Duration::from_secs(5));
+	    		("HTTP/1.1 200 OK", "hello.html")
+	    	} else {
+	    		("HTTP/1.1 404 NOT FOUND", "404.html")
+	    	}
+    	;
 
 		let contents = fs::read_to_string(filename).unwrap();
 
