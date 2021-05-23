dbbe4bee162e2f36f58b3dd04fdafde0b9633c5e
Michael S. Huang <mhuang74@gmail.com>
2021-05-23 08:05:06 +0800

    added graceful shutdown via ctrl-c

--- rust-tutorials/simple_webserver/src/bin/main.rs
+++ rust-tutorials/simple_webserver/src/bin/main.rs
@@ -5,6 +5,9 @@ use std::fs;
 use std::thread;
 use std::time::Duration;
 use simple_webserver::ThreadPool;
+use std::sync::atomic::{AtomicBool, Ordering};
+use std::sync::Arc;
+
 
 fn main() {
 	const SERVER_ADDRESS : &str = "127.0.0.1:7878";
@@ -13,15 +16,43 @@ fn main() {
 
 	let listener = TcpListener::bind(SERVER_ADDRESS).unwrap();
 
-	let pool = ThreadPool::new(4);
+	let mut pool = ThreadPool::new(4);
 
+	let shutdown = Arc::new(AtomicBool::new(false));
+	let s = shutdown.clone();
+
+	//  Ctrl-C sends Termination
+	ctrlc::set_handler(move || {
+
+		s.store(true, Ordering::SeqCst);
+
+		println!("Ctrl-C detected. Shutdown flag set.");
+
+	}).expect("Error setting Ctrl-C handler");
+
+	// equivalent to calling listner.accept() in a loop
+	// accept() blocks caller until new TCP connection is established
 	for stream in listener.incoming() {
+
+		// check if need terminate first
+		if shutdown.load(Ordering::SeqCst) {
+			println!("Commencing shutdown...");
+
+			pool.terminate();
+
+			break;
+		} 
+
 		let stream = stream.unwrap();
 
 		pool.execute(|| {
 			handle_connection(stream);
 		});
+
+
 	}
+
+
 
     fn handle_connection(mut stream: TcpStream) {
 
@@ -29,7 +60,8 @@ fn main() {
 
     	stream.read(&mut buffer).unwrap();
 
-	    println!("Thread: {:?} - Request: {}", thread::current().id(), String::from_utf8_lossy(&buffer[..]));
+	    // println!("Thread: {:?} - Request: {}", thread::current().id(), String::from_utf8_lossy(&buffer[..]));
+		println!("Thread: {:?} - Got Request", thread::current().id());
 
     	let get = b"GET / HTTP/1.1\r\n";
     	let sleep = b"GET /sleep HTTP/1.1\r\n";
@@ -38,7 +70,7 @@ fn main() {
 	    	if buffer.starts_with(get) {
 	    		("HTTP/1.1 200 OK", "hello.html")
 	    	} else if buffer.starts_with(sleep) {
-	    		thread::sleep(Duration::from_secs(5));
+	    		thread::sleep(Duration::from_secs(1));
 	    		("HTTP/1.1 200 OK", "hello.html")
 	    	} else {
 	    		("HTTP/1.1 404 NOT FOUND", "404.html")
--- rust-tutorials/simple_webserver/src/lib.rs
+++ rust-tutorials/simple_webserver/src/lib.rs
@@ -5,11 +5,16 @@ use std::sync::Mutex;
 
 type Job = Box<dyn FnOnce() + Send + 'static>;
 
+enum Message {
+	NewJob(Job),
+	Terminate
+}
+
 pub struct ThreadPool {
 	// our closure passed to thread::spawn() won't return anything,
 	// so return type of unit type () is used by JoinHandle<T>
 	workers: Vec<Worker>,
-	sender: mpsc::Sender<Job>,
+	sender: mpsc::Sender<Message>,
 }
 
 impl ThreadPool {
@@ -29,11 +34,12 @@ impl ThreadPool {
 
 		let rx = Arc::new(Mutex::new(rx));
 
-		for idx in 0..size {
+		for idx in 1..size+1 {
 			workers.push(Worker::new(idx, Arc::clone(&rx)));
 		}
 
-		ThreadPool { workers, sender:tx }
+
+		ThreadPool { workers, sender:tx }	
 	}
 
 	pub fn execute<F>(&self, f: F) 
@@ -43,31 +49,73 @@ impl ThreadPool {
 
 		let job = Box::new(f);
 
-		self.sender.send(job).unwrap();
+		self.sender.send(Message::NewJob(job)).unwrap();
 
+		println!("Request sent to Worker channel");
+
+	}
+
+	pub fn terminate(&mut self) {
+
+		println!("Sending terminate message to all workers.");
+
+		// send any many Terminate msssage as there are Workers
+		for _ in &self.workers {
+			self.sender.send(Message::Terminate).unwrap();
+		}
+
+		println!("Shutting down all workers.");
+
+		for worker in &mut self.workers {
+			println!("Shutting down worker {}", worker.id);
+
+			if let Some(thread) = worker.thread.take() {
+				thread.join().unwrap();
+			}
+		}
+	}	
+}
+
+impl Drop for ThreadPool {
+	fn drop(&mut self) {
+		println!("Dropping off..bye bye!")
 	}
 }
 
 struct Worker {
 	id: usize,
-	thread: thread::JoinHandle<()>,
+	thread: Option<thread::JoinHandle<()>>,
 }
 
 impl Worker {
 	/// Create a new Worker.
 	///
-	fn new(id: usize, receiver: Arc<Mutex<mpsc::Receiver<Job>>>) -> Worker {
+	fn new(id: usize, receiver: Arc<Mutex<mpsc::Receiver<Message>>>) -> Worker {
 
 		// create new thread with empty closure
 		let thread = thread::spawn(move || loop {
-			let job = receiver.lock().unwrap().recv().unwrap();
 
-			println!("Worker {} got a job; executing.", id);
+			let message = receiver.lock().unwrap().recv().unwrap();
 
-			job();
+			match message {
+				Message::NewJob(job) => {
+
+					println!("Worker {} received new job; executing.", id);
+
+					job();
+				}
+				Message::Terminate => {
+
+					println!("Worker {} was told to TERMINATE.", id);
+
+					break;
+
+				}
+			}
+
 		});
 
-		Worker { id, thread }
+		Worker { id, thread: Some(thread) }
 	}
 }
 
