e2e39a57299d268b08b1a454b97a66aae5164b35
Michael S. Huang <mhuang74@gmail.com>
2021-08-15 18:38:47 +0800

    works for esp32-cam

---
+++ platformio-tutorial/espidf/blink_esp32/README.md
@@ -0,0 +1,11 @@
+This project combines esp-idf blink and hello_world examples:
+
+https://github.com/espressif/esp-idf/tree/v4.3/examples/get-started/blink
+https://github.com/espressif/esp-idf/tree/v4.3/examples/get-started/hello_world
+
+PlatformIO Project created via
+
+```
+pio project init -b esp32cam --ide vscode --project-option="framework=espidf"
+```
+
---
+++ platformio-tutorial/espidf/blink_esp32/src/main.c
@@ -0,0 +1,75 @@
+/* Blink Example
+   This example code is in the Public Domain (or CC0 licensed, at your option.)
+   Unless required by applicable law or agreed to in writing, this
+   software is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
+   CONDITIONS OF ANY KIND, either express or implied.
+*/
+#include <stdio.h>
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "driver/gpio.h"
+#include "sdkconfig.h"
+
+#include "esp_system.h"
+#include "esp_spi_flash.h"
+
+/* Can use project configuration menu (idf.py menuconfig) to choose the GPIO to blink,
+   or you can edit the following line and set a number here.
+*/
+#define BLINK_GPIO CONFIG_BLINK_GPIO
+
+void app_main(void)
+{
+
+    printf("Hello world!\n");
+
+    /* Print chip information */
+    esp_chip_info_t chip_info;
+    esp_chip_info(&chip_info);
+    printf("This is %s chip with %d CPU core(s), WiFi%s%s, ",
+            CONFIG_IDF_TARGET,
+            chip_info.cores,
+            (chip_info.features & CHIP_FEATURE_BT) ? "/BT" : "",
+            (chip_info.features & CHIP_FEATURE_BLE) ? "/BLE" : "");
+
+    printf("silicon revision %d, ", chip_info.revision);
+
+    printf("%dMB %s flash\n", spi_flash_get_chip_size() / (1024 * 1024),
+            (chip_info.features & CHIP_FEATURE_EMB_FLASH) ? "embedded" : "external");
+
+    printf("Minimum free heap size: %d bytes\n", esp_get_minimum_free_heap_size());
+
+
+
+    /* Configure the IOMUX register for pad BLINK_GPIO (some pads are
+       muxed to GPIO on reset already, but some default to other
+       functions and need to be switched to GPIO. Consult the
+       Technical Reference for a list of pads and their default
+       functions.)
+    */
+    gpio_reset_pin(BLINK_GPIO);
+    /* Set the GPIO as a push/pull output */
+    gpio_set_direction(BLINK_GPIO, GPIO_MODE_OUTPUT);
+
+    for (int i = 10; i >= 0; i--) {
+
+        printf("Restarting in %d seconds...\n", i);
+
+        /* Blink off (output low) */
+        printf("Turning off the LED\n");
+        gpio_set_level(BLINK_GPIO, 0);
+        vTaskDelay(500 / portTICK_PERIOD_MS);
+        /* Blink on (output high) */
+        printf("Turning on the LED\n");
+        gpio_set_level(BLINK_GPIO, 1);
+        vTaskDelay(500 / portTICK_PERIOD_MS);
+
+    }
+    printf("Restarting now.\n");
+    fflush(stdout);
+    esp_restart();
+
+    while(1) {
+
+    }
+}
