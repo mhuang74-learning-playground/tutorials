a59b4b9254e2cdd9e07e6074fe804e320cdcf31e
Michael S. Huang <mhuang74@gmail.com>
2021-11-06 09:17:33 +0800

    add hello-world-esp32-qemu; works with esp-idf v4.4

---
+++ rust-esp-tutorials/hello-world-esp32-qemu/qemu.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+# You might need to change this...
+ESP_QEMU_PATH=$HOME/Projects/3rd-party/espressif/qemu/build
+BUILD=debug
+# use same esp-idf env as esp-idf-sys library
+ESPTOOL_CMD="python3 $HOME/.espressif/esp-idf-release/v4.4/components/esptool_py/esptool/esptool.py"
+
+TARGET=xtensa-esp32-espidf # Don't change this. Only the ESP32 chip is supported in QEMU for now
+
+$ESPTOOL_CMD --chip esp32 elf2image target/$TARGET/$BUILD/hello-world-esp-rs
+$ESPTOOL_CMD --chip esp32 merge_bin --output target/$TARGET/$BUILD/hello-world-esp-rs-qemu.bin --fill-flash-size 4MB 0x1000 qemu_bins/bootloader.bin  0x8000 qemu_bins/partitions.bin  0x10000 target/$TARGET/$BUILD/hello-world-esp-rs.bin --flash_mode dio --flash_freq 40m --flash_size 4MB
+$ESP_QEMU_PATH/qemu-system-xtensa -nographic -machine esp32 -nic user,model=open_eth,id=lo0,hostfwd=tcp:127.0.0.1:7888-:80 -drive file=target/$TARGET/$BUILD/hello-world-esp-rs-qemu.bin,if=mtd,format=raw -m 4M
